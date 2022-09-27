6367851d7b3f828f7dc758bb129e1b441bc42aed
ZEE <Zaidsh02@gmail.com>
2022-09-27 19:56:30 +0200

    Player class start up

--- Player.java
+++ Player.java
@@ -1,2 +1,9 @@
+import java.util.Scanner;
+
 public class Player {
+
+    public Player(int playerNum) {
+        Scanner in = new Scanner(System.in);
+        System.out.println(String.format("Player %d. Please enter your name", playerNum));
+    }
 }
