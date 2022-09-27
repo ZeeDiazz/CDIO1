c1f4e49d40b613dc92a6315d8b9f2775af9364a0
ZEE <Zaidsh02@gmail.com>
2022-09-27 21:31:55 +0200

    Player constructor

--- Player.java
+++ Player.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
 
 public class Player {
-
+    public final String Name;
+    private int currentScore;
     public Player(int playerNum) {
         Scanner in = new Scanner(System.in);
         System.out.println(String.format("Player %d. Please enter your name", playerNum));
+        this.Name = in.nextLine();
+        this.currentScore = 0;
     }
 }
