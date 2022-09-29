365922532adb47e3486a22b1054d396a69b86e08
Zigalow <s224302@dtu.dk>
2022-09-29 12:01:46 +0200

    Tilføjet Die-klassen. Lavet en constructor for klassen

---
+++ Die.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+public class Die {
+
+    private int faceValue;
+    private Random rng;
+
+    //Contructor for class.
+    public Die(){
+        rng = new Random();
+    }
+}
