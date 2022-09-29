9056b9a0f676a8938f243ce8621e9f01d8ad2ec4
Daniel <danielweper@gmail.com>
2022-09-29 14:17:28 +0200

    Made a main function and fixed printDiceResult

--- Game.java
+++ Game.java
@@ -1,25 +1,31 @@
-public class Game {
+Public class Game {   
+        private Die die1, die2;
 
-    private Die die1, die2;
 
-    public Game() {
-        die1 = new Die();
-        die2 = new Die();
-    }
+       public Game() {
+               die1 = new die();
+               die2 = new die();
+       }
 
-    public void playRound() {
-        die1.roll();
-        die2.roll();
-    }
+       public static void main(String[] args) {
+               for (int i = 0; i < 10; i++) {
+                       playRound();
+                       printDiceResults();
+              } 
+      }
+
+
+        public int playRound() {
+                die1.roll();
+                die2.roll();
+        }
+
+        public int diceSum() {
+                return die1.getFaceValue() + die2.getFaceValue();
+        }
 
         public void printDiceResult() {
-            System.out.println("You rolled: %d & %d" + die1.getFaceValue() + die2.getFaceValue());
+                System.out.println(“You rolled: %d & %d”, die1.getFaceValue(), die2.getFaceValue());
+                System.out.println(String.format(“You rolled: %d & %d”, die1.getFaceValue(), die2.getFaceValue()));
         }
-
-        public static void main (String[]args){
-            Player[] players = new Player[2]; //Der der skal være to spiller
-        }
-
-
-
 }
