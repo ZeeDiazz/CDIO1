c3df13e9eb8339264e953a70519de15408f1f24f
Daniel Weper Jensen <danielweper@gmail.com>
2022-09-30 11:58:06 +0200

    Added unit tests

--- DieTest.java
+++ DieTest.java
@@ -1,4 +1,28 @@
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class DieTest {
-  
+    @Test
+    public void dieRollTakesLessThan10ms() {
+        Die d = new Die();
+        for (int i = 0; i < 1000; i++) {
+            assertTimeout(Duration.ofMillis(10), d::roll);
+        }
+    }
+
+    @Test
+    public void dieFaceCanChangeValue() {
+        Die d = new Die();
+        d.roll();
+        int startValue = d.getFaceValue();
+
+        assertTimeout(Duration.ofSeconds(1), () -> {
+            while (startValue == d.getFaceValue()) {
+                d.roll();
+            }
+        });
+    }
 }
